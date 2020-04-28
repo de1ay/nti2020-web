efc66cb052d2ccbfbf6dc7f824239b361daf1264
Vsevolod Kosarev <54035308+iamvkosarev@users.noreply.github.com>
2020-04-28 16:52:24 +0300

    Front-end Чат 0.2
    Сделал стили для боковой панели со всеми чатами.  Оформил header отдельного чата. Начал работу над лентой сообщений

---
+++ src/views/base.vue
@@ -0,0 +1,10 @@
+Vue.component('button-counter', {
+  data: function () {
+    return {
+      count: 0
+    }
+  },
+  template: '<button v-on:click="count++">Счётчик кликов — {{ count }}</button>'
+})
+
+new Vue({ el: '#components-demo' })
